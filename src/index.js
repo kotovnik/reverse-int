df1858a409aebd82393287ea48472df8d90336d4
kotovnik <kotovnik2024tbilisi@gmail.com>
2023-08-07 07:36:43 +0400

    feat: add solved task by cycle for

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = String(n);
+    let result = '';
+    for (i = 0; i < str.length; i++) {
+        
+        if (str[i] === '-') {
+            result = '' + result;
+        } else {
+            result = str[i] + result;
+        }
+    }
+    return +result;
+};
